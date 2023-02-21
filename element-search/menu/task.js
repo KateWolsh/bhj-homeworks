446353f170417a28143656a4294b0d5fdebfd469
Kate <katewolshanat@gmail.com>
2023-02-21 15:31:32 +0100

    task completed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,19 +1,37 @@
-const menuLink = Array.from(document.getElementsByClassName("menu__link"));
-const menu = Array.from(document.getElementsByClassName("menu menu_sub"));
-const item = Array.from(document.getElementsByClassName("menu__item"));
+ 
+    const menuLink = Array.from(document.querySelectorAll(".menu__link"));
+    const menu = Array.from(document.querySelectorAll(".menu.menu_sub"));
+    const item = Array.from(document.querySelectorAll(".menu__item"));
 
-
-
-for (let index1 = 0; index1 < menuLink.length; index1++) {
-    menuLink[index1].onclick = () => {
-        for (let index = 0; index < item.length; index++) {
-            const menuMenu = item[index].querySelector(".menu.menu_sub");
-            if(menuMenu != null){
-                item[index].onclick = () =>{
-                menuMenu.classList.add("menu_active");
+    for (let index1 = 0; index1 < menuLink.length; index1++) {     //перебираем циклом ВСЕ пункты меню
+    menuLink[index1].onclick = () => {                                 //устанавливаем событие клик на пункты меню
+        for (let index = 0; index < item.length; index++) {               //перебираем циклом только вложенные пункты меню
+            const menuMenu = item[index].querySelector(".menu.menu_sub"); 
+            if(menuMenu != null){                                           //если в главном меню есть выпадающих список, то активируем его 
+                item[index].onclick = () =>{                                // при клике выпадающее меню активируется
+                menuMenu.classList.toggle("menu_active");
                 return false;
             }
         }
         }      
     }
 }
+
+
+
+
+// item[index].addEventListener("click", function(){
+//     for (let index = 0; index < item.length; index++){
+//         if(item[index].closest(".menu.menu_sub") !== null){
+//           menuMenu.classList.toggle("menu_active");
+//           return false;
+//         }
+//     }
+// })
+
+
+
+
+
+
+
