6c97521b8fc459de33e5d74ac8229729f370a034
Kate <katewolshanat@gmail.com>
2023-02-22 15:27:20 +0100

    task completed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,37 +1,15 @@
  
-    const menuLink = Array.from(document.querySelectorAll(".menu__link"));
-    const menu = Array.from(document.querySelectorAll(".menu.menu_sub"));
-    const item = Array.from(document.querySelectorAll(".menu__item"));
+const menuLinks = Array.from(document.querySelectorAll(".menu__link")); // All 'a' elements
+const menu = Array.from(document.querySelectorAll(".menu.menu_sub")); // Ul element
+const listItems = Array.from(document.querySelectorAll(".menu__item")); // Li element
 
-    for (let index1 = 0; index1 < menuLink.length; index1++) {     //перебираем циклом ВСЕ пункты меню
-    menuLink[index1].onclick = () => {                                 //устанавливаем событие клик на пункты меню
-        for (let index = 0; index < item.length; index++) {               //перебираем циклом только вложенные пункты меню
-            const menuMenu = item[index].querySelector(".menu.menu_sub"); 
-            if(menuMenu != null){                                           //если в главном меню есть выпадающих список, то активируем его 
-                item[index].onclick = () =>{                                // при клике выпадающее меню активируется
-                menuMenu.classList.toggle("menu_active");
-                return false;
-            }
+for (let index = 0; index < listItems.length; index++) { 
+  const link = listItems[index].querySelector(".menu__link");          //перебираем циклом только вложенные пункты меню
+    const menuMenu = listItems[index].querySelector(".menu.menu_sub");
+    if (menuMenu !== null && link !== null) {                                          //если в главном меню есть выпадающих список, то активируем его 
+        link.onclick = () => {                                // при клике выпадающее меню активируется
+          menuMenu.classList.toggle("menu_active");
+          return false;
         }
-        }      
     }
 }
-
-
-
-
-// item[index].addEventListener("click", function(){
-//     for (let index = 0; index < item.length; index++){
-//         if(item[index].closest(".menu.menu_sub") !== null){
-//           menuMenu.classList.toggle("menu_active");
-//           return false;
-//         }
-//     }
-// })
-
-
-
-
-
-
-
