b57cb6f3aaee272be1498f150083a6a714761869
Kate <katewolshanat@gmail.com>
2023-03-03 11:16:03 +0100

    task completed

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -12,7 +12,25 @@ for (let i = 0; i < fonts.length; i++) {
             fontsChildren[t].classList.remove("font-size_active");
         }
         fonts[i].classList.add("font-size_active");
-    });
-
+        event.preventDefault();
+    }, false);
+    
 }
+
+for (let i = 0; i < fonts.length; i++) {
+    fonts[i].addEventListener("click", () => {
+      if(fonts[i].classList.contains("font-size_small")){
+        book.classList.remove("book_fs-big");
+        book.classList.add("book_fs-small");
+      }
+      else if(fonts[i].classList.contains("font-size_big")){
+        book.classList.remove("book_fs-small");
+        book.classList.add("book_fs-big");
+      }else{
+        book.classList.remove("book_fs-small");
+        book.classList.remove("book_fs-big");
+      }
+      
+    }, false);
+}   
 
