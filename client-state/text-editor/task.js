145cfe9e015ea73a66601e8189284e3e0b30375d
Kate <katewolshanat@gmail.com>
2023-03-30 14:03:13 +0200

    text-editor completed

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,9 +1,7 @@
 const editor = document.getElementById('editor');
 
-// при загрузке страницы проверяем, есть ли сохраненный текст в локальном хранилище
-if (localStorage.getItem('text')) {
+// сохраняем текст в localStorage
   editor.value = localStorage.getItem('text');
-}
 
 // сохраняем текст в локальном хранилище при каждом изменении в текстовом редакторе
 editor.addEventListener('input', () => {
