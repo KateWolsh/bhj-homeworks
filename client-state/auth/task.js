d888e983a92a7d61e55e4325344f09719a7f58b6
Kate <katewolshanat@gmail.com>
2023-03-30 14:02:20 +0200

    auth completed

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,34 +1,43 @@
-
-//Получаем доступ к форме HTML
+//Получаем доступ к HTML-элементам
 const form = document.getElementById('signin__form');
+const formBlock = document.querySelector('.signin');
+const welcomeBlock = document.getElementById('welcome');
+const userId = localStorage.getItem('userId');
+const userIdSpan = welcomeBlock.querySelector('#user_id');
+
+//проверяем есть ли в LocalStorage ID пользователя
+if (userId) {
+  userIdSpan.textContent = userId;
+  welcomeBlock.classList.add('welcome_active');
+  formBlock.classList.remove('signin_active');
+}
+
 //Навешиваем слушателя к событию отправки формы
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+ //отправляем POST запрос
+  const formData = new FormData(form);
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+  xhr.send(formData);
 
-  //отправляем POST запрос
-    const formData = new FormData(form);
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-    xhr.send(formData);
-    xhr.onreadystatechange = function() {
-    if (this.readyState === XMLHttpRequest.DONE) {
-      if (this.status === 200) {
-
-        // Если запрос успешный, то сохраняем ID
-        localStorage.setItem('userId', response.user_id);
-
-        // Выводим блок об успешной авторизации
-        const welcomeBlock = document.getElementById('welcome');
-        const userIdSpan = welcomeBlock.querySelector('#user_id');
-        userIdSpan.textContent = response.user_id;
-        welcomeBlock.classList.add('welcome_active');
-      } else {
+  xhr.onload = function() {
+    // Если запрос успешный, то сохраняем ID
+    if (xhr.response.success == true) {
+      const response = xhr.response;
+      localStorage.setItem('userId', response.user_id);
+      form.reset();
+    // Выводим блок об успешной авторизации
+      userIdSpan.textContent = response.user_id;
+      welcomeBlock.classList.add('welcome_active');
+      formBlock.classList.remove('signin_active');
+    } else {
         // В случае неуспешной авторизации выводим сообщение об ошибке
         const errorMessage = document.createElement('p');
         errorMessage.textContent = 'Неверный логин/пароль';
         form.appendChild(errorMessage);
-      }
     }
   };
-  xhr.send(formData);
 });
+
